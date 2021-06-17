b5b7c1e38b070e9734740caac39bbda15fb4f6e6
socurites <socurites@wise-fashion.com>
2021-06-17 18:01:58 +0900

    Studying..

---
+++ src/main/java/com/socurites/modern/stream/basic/Dish.java
@@ -0,0 +1,34 @@
+package com.socurites.modern.stream.basic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Dish {
+	  private final String name;
+	  private final boolean vegetarian;
+	  private final int calories;
+	  private final Type type;
+
+	  public enum Type {
+	    MEAT,
+	    FISH,
+	    OTHER
+	  }
+
+	  public static final List<Dish> menu = Arrays.asList(
+	      new Dish("pork", false, 800, Dish.Type.MEAT),
+	      new Dish("beef", false, 700, Dish.Type.MEAT),
+	      new Dish("chicken", false, 400, Dish.Type.MEAT),
+	      new Dish("french fries", true, 530, Dish.Type.OTHER),
+	      new Dish("rice", true, 350, Dish.Type.OTHER),
+	      new Dish("season fruit", true, 120, Dish.Type.OTHER),
+	      new Dish("pizza", true, 550, Dish.Type.OTHER),
+	      new Dish("prawns", false, 400, Dish.Type.FISH),
+	      new Dish("salmon", false, 450, Dish.Type.FISH)
+	  );
+}
---
+++ src/main/java/com/socurites/modern/stream/basic/FilterAndSortDishes.java
@@ -0,0 +1,15 @@
+package com.socurites.modern.stream.basic;
+
+/**
+ * Dish 객체 리스트를 
+ * 1. 필터링
+ * 2. 정렬
+ * 
+ * 하는 방식에 대한 7버전과 8버전의 비교
+ * 
+ * @author socurites
+ *
+ */
+public class FilterAndSortDishes {
+
+}
