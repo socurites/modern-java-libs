bdbc2e3bafd4af7da4804b0c2dc9609a02608702
socurites <socurites@wise-fashion.com>
2021-06-17 10:25:00 +0900

    First Commit

---
+++ src/main/java/com/socurites/modern/ModernJavaLibsApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.modern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaLibsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socurites/modern/ModernJavaLibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socurites.modern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaLibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
