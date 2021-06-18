4b9c29a8ace8b7280bf13a2c4ecf0597e8e6a697
socurites <socurites@wise-fashion.com>
2021-06-18 11:47:29 +0900

    Chap 4

--- README.md
+++ README.md
@@ -9,9 +9,38 @@
 
 
 ### 신규 기능
+* @FunctionalInterace
+
+| @FunctionalInterface  | Function |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+
 * 스트림 API
     * 선언형 프로그래밍 like SQL
     * 병렬연산 지원
+    * 조립할 수 있음
+        * 중간 연산
+            * 필터링: filter
+            * 슬라이싱: takeWhile, dropWhile
+            * 정렬: sorted
+            * 검색: find
+            * 매칭: match
+            * 매핑: map
+            * 리듀싱: reduce 
+            * 축소: limit
+            * 중복제거: distinct
+        * 단말(최종) 연산
+            * 수집: collect
+            * 갯수: count
+            * 방문: foEach
+        
+    * 기타 특징
+        * 게으른 연산
+        * 쇼트서킷(short circuit)
+        * 루프 퓨전(loop fusion)
+        
 
 * 메서드에 코드를 전달(메서드 참조 & 람다)
     * 동작 파라미터화(behavior parameterization) 
--- src/main/java/com/socurites/modern/stream/basic/FilterAndSortDishes.java
+++ src/main/java/com/socurites/modern/stream/basic/FilterAndSortDishes.java
@@ -1,5 +1,11 @@
 package com.socurites.modern.stream.basic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Dish 객체 리스트를 
  * 1. 필터링
@@ -11,5 +17,57 @@ package com.socurites.modern.stream.basic;
  *
  */
 public class FilterAndSortDishes {
-
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public void exampleBefore001() {
+		List<Dish> menu = Dish.menu;
+		
+		List<Dish> lowCaloricDishes = new ArrayList<>();		// <-- 컨테이너 역할만 하는 중간 변수
+		
+		// Filtering by condition
+		for(Dish dish : menu) {
+			if(dish.getCalories() < 400) {
+				lowCaloricDishes.add(dish);
+			}
+		}
+		
+		// Sorting
+		Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+			@Override
+			public int compare(Dish o1, Dish o2) {
+				return Integer.compare(o1.getCalories(), o2.getCalories());
+			}
+			
+		});
+		
+		// Mapping A -> B
+		List<String> lowCaloricDishesName = new ArrayList<>();	// <-- 최종 결과
+		for(Dish dish : lowCaloricDishes) {
+			lowCaloricDishesName.add(dish.getName());
+		}
+	}
+	
+	
+	public void exampleAfter001() {
+		List<Dish> menu = Dish.menu;
+		
+		List<String> lowCaloricDishesName = menu.stream()
+				.filter(d -> d.getCalories() < 400)
+				.sorted(Comparator.comparing(Dish::getCalories))
+				.map(Dish::getName)
+				.collect(Collectors.toList());
+	}
+	
+	public void exampleAfter002() {
+		List<Dish> menu = Dish.menu;
+		
+		List<String> lowCaloricDishesName = menu.parallelStream()	// <-- 병렬
+				.filter(d -> d.getCalories() < 400)
+				.sorted(Comparator.comparing(Dish::getCalories))
+				.map(Dish::getName)
+				.collect(Collectors.toList());
+	}
 }
