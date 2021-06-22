b27c47e668b6d35b739a1a0ccd6d98c9c6dff921
socurites <socurites@wise-fashion.com>
2021-06-22 13:01:40 +0900

    chap9

---
+++ src/main/java/com/socurites/modern/usages/ReafactoringBasicEx.java
@@ -0,0 +1,21 @@
+package com.socurites.modern.usages;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReafactoringBasicEx {
+	
+	public static void main(String[] args) {
+		// Refactoring
+		// 1. 익명 클래스 ===> 람다 표현식
+		// 2. 람다 표현식 ===> 메서드 참조
+		// 3. 명령형 구문 ===> stream 선언형 구문
+		
+		
+		// Custom 함수 인터페이스
+		// 1. 조건부 연기실행
+		// 2. 실행 어라운드
+	}
+	
+
+}
---
+++ src/main/java/com/socurites/modern/usages/StrategyPatternEx.java
@@ -0,0 +1,5 @@
+package com.socurites.modern.usages;
+
+public class StrategyPatternEx {
+
+}
